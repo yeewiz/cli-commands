51a624fc5904965ee5241211dc7b33e45081b780
Yee Fung <Yee@dws.la>
2018-03-30 16:25:17 -0700

    seeing different arguments

--- command.php
+++ command.php
@@ -1,8 +1,16 @@
 <?php
 
 class Foo_Command {
-    public function __invoke( $args ) {
-        WP_CLI::success( 'hello ' . $args[0] );
-    }
+  public function __invoke( $args = array(), $assoc_args = array() ) {
+    $arguments = wp_parse_args( $assoc_args, array(
+      'start' => '',
+      'end' => '',
+    ) );
+    
+    WP_CLI::line( $arguments['start'] . ' ' . $arguments['end'] );
+  }
 }
-WP_CLI::add_command( 'foo', 'Foo_Command' );
+
+if ( defined( 'WP_CLI' ) && WP_CLI ) {
+  WP_CLI::add_command( 'foo', 'Foo_Command' );
+}
