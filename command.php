fc3770bf262e2e84b2282c28f1edc7f414219901
Yee Fung <Yee@dws.la>
2018-03-29 19:48:44 -0700

    added additional to see update

--- command.php
+++ command.php
@@ -2,7 +2,7 @@
 
 class Foo_Command {
     public function __invoke( $args ) {
-        WP_CLI::success( $args[0] );
+        WP_CLI::success( 'hello' . $args[0] );
     }
 }
 WP_CLI::add_command( 'foo', 'Foo_Command' );
